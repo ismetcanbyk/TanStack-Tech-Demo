e9c1a836f00997a2ab6e8173f3d64af91889172e
İSMET CAN BIYIK <91330952+ismetcanbyk@users.noreply.github.com>
2025-07-24 17:56:25 +0300

    feat: edited header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,9 @@ export function Header() {
   return (
     <header style={{ padding: 20, background: "#eee" }}>
       <div className="flex justify-between items-center">
-        <h1>Tanstack Tech Demo</h1>
+        <Link to="/">
+          <h1>Tanstack Tech Demo</h1>
+        </Link>
         <div className="flex gap-4">
           <Link to="/">Home</Link>
           <Link to="/pokemon">Pokemon</Link>
